2975cd8b6f71ae7928daddde0459079b7e2a1555
William Gibb <william.gibb@fireeye.com>
2015-10-30 08:40:59 -0400

    Found a straggler example script that neeed logging updates.

--- examples/openioc_to_yara/check_yara_rules.py
+++ examples/openioc_to_yara/check_yara_rules.py
@@ -24,36 +24,37 @@ import sys
 import logging
 import optparse
 
-# logging config
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s  [%(filename)s:%(funcName)s]')
+log = logging.getLogger(__name__)
+
 
 try:
     import yara
-except ImportError, e:
-    logging.error('Could not import yara')
+except ImportError:
+    log.exception('Could not import yara')
     sys.exit(1)
     
 def check_rules(rules, fp):
     matches = rules.match(fp)
     if matches:
-        logging.info('Matched [%s] to %s' % (os.path.basename(fp), str(matches)))
+        log.info('Matched [%s] to %s' % (os.path.basename(fp), str(matches)))
         return True
     else:
-        logging.debug('No match for [%s]' % os.path.basename(fp))
+        log.debug('No match for [%s]' % os.path.basename(fp))
         return False
     
 def main(options):
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s  [%(filename)s:%(funcName)s]')
     if not options.verbose:
         logging.disable(logging.DEBUG)
     
     if not os.path.isfile(options.yara):
-        logging.error('Yara rules file is not a file')
+        log.error('Yara rules file is not a file')
         sys.exit(1)
     
     try:
         rules = yara.compile(options.yara)
-    except yara.SyntaxError, e:
-        logging.error('Failed to process rules. [%s]' % str(e))
+    except yara.SyntaxError:
+        log.exception('Failed to process rules.')
         sys.exit(1)
         
     path = options.fp
@@ -65,7 +66,7 @@ def main(options):
             if os.path.isfile(fp):
                 check_rules(rules, fp)
     else:
-        logging.error('input is not a file or a directory')
+        log.error('input is not a file or a directory')
         sys.exit(1)
     sys.exit(0)
     
@@ -86,12 +87,12 @@ Test a yara rule against a set of files, or file."""
     options, args = parser.parse_args()
 
     if not options.yara:
-        logging.error('Must specify a yara rules file to process.')
+        log.error('Must specify a yara rules file to process.')
         parser.print_help()
         sys.exit(1)
 
     if not options.fp:
-        logging.error('Must specify a file or directory to process.')
+        log.error('Must specify a file or directory to process.')
         parser.print_help()
         sys.exit(1)
 
