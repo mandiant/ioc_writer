df705e5a0ffef03d703539159429a785897e2546
William Gibb <william.gibb@fireeye.com>
2015-10-30 11:08:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
-#This is a list of files to install, and where
-#(relative to the 'root' dir, where setup.py is)
-#You could be more specific.
-files = ["ioc_writer/*"]
 
-setup(name = "ioc_writer",
-    version = "0.1.1",
-    description = "",
-    author = "William Gibb",
-    author_email = "william.gibb@mandiant.com",
-    url = "http://www.github.com/mandiant/iocwriter_11/",
-    packages = ['ioc_writer'],
-    package_data = {'package' : files },
-    #scripts = ["runner"],
-    long_description = """API providing a limited CRUD for manipulating OpenIOC formatted Indicators of Compromise.""",
-    #This next part it for the Cheese Shop, look a little down the page.
-    classifiers=[
-      'Development Status :: 4 - Beta',
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'Intended Audience :: Information Technology',
-      'Intended Audience :: System Administrators',
-      'License :: OSI Approved :: Apache Software License',
-      'Natural Language :: English',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Topic :: Security',
-      'Topic :: Text Processing :: Markup :: XML'
-      ]  
-) 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="ioc_writer",
+      version="0.2.0",
+      author="William Gibb",
+      author_email="william.gibb@mandiant.com",
+      url="http://www.github.com/mandiant/iocwriter_11/",
+      packages=['ioc_writer'],
+      description="""API providing a limited CRUD for manipulating OpenIOC formatted Indicators of Compromise.""",
+      long_description=read('README'),
+      install_requires=['lxml'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Security',
+          'Topic :: Text Processing :: Markup :: XML'
+      ]
+      )
